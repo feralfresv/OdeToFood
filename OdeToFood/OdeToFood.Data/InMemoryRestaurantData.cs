02526742a8dc413515f94e60d035e6a67c07ac58
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-03-26 03:27:25 -0600

    V1.2 9 - Deploying ASP.NET Core Web Applications

---
+++ OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,75 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id= 1, Name="Luigi Pizza", Location="Roma", Cuisine= CuisineType.Italian },
+                new Restaurant{Id= 2, Name="Pelana Tacos", Location="Mexico", Cuisine= CuisineType.Mexican },
+                new Restaurant{Id= 3, Name="Mario Pizza", Location="Milan", Cuisine= CuisineType.Italian }
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+
+        }
+
+        public Restaurant Update(Restaurant updateRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updateRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updateRestaurant.Name;
+                restaurant.Location = updateRestaurant.Location;
+                restaurant.Cuisine = updateRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from d in restaurants
+                   where string.IsNullOrEmpty(name) || d.Name.StartsWith(name)
+                   orderby d.Name
+                   select d;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+using OdeToFood.Core;
+using Microsoft.Extensions.Logging;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+
+        public string Messagge { get; set; }
+        public IEnumerable<Core.Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData,
+                         ILogger<ListModel> logger)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+            logger.LogError("***Executing ListModel***");
+            Messagge = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
